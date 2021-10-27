2a7aab05e959da81c24efe160077e15b27fd9098
chandan9074 <chandan15-11611@diu.edu.bd>
2021-10-27 19:49:54 +0600

    feat: connent mongodb-express and react-node

--- mongodb-express-nodejs-server/index.js
+++ mongodb-express-nodejs-server/index.js
@@ -1,27 +1,39 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 
+const cors = require("cors");  
+
 const app = express();
 const port = 5000;
 
 // user : mymongodb
 // password : q7EvqdJo9GxtiNjI
 
+app.use(cors())
+app.use(express.json())
 
 const uri = "mongodb+srv://mymongodb:q7EvqdJo9GxtiNjI@cluster0.z2qxz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 async function run() {
   try {
     await client.connect();
-    const database = client.db("insertDB");
+    const database = client.db("mongoUserPractice");
     const userCollection = database.collection("users");
     // create a document to insert
-    const doc = {
-      name: "chandan kumar",
-      email: "chandan@gmail.com",
-    }
-    const result = await userCollection.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    // const doc = {
+    //   name: "chandan kumar",
+    //   email: "chandan@gmail.com",
+    // }
+    // const result = await userCollection.insertOne(doc);
+    // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    
+    // post api
+    app.post('/users', async (req, res)=>{
+      console.log("here krishna", req.body);
+    })
+
   } finally {
     await client.close();
   }
