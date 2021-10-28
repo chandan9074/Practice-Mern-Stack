b4ce717c606a0ffc462df85f1d0dfb895907c7c3
chandan9074 <chandan15-11611@diu.edu.bd>
2021-10-28 17:50:33 +0600

    Project setup Genius car machanics

---
+++ Genius car machanics/Genius-Car-Mechanics-module-61/src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,21 @@
+console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyAw-7dFfV-ZrvX19qvwVB6x1QKpl8RrUqo",
+  authDomain: "simple-firebase-authenti-c309b.firebaseapp.com",
+  projectId: "simple-firebase-authenti-c309b",
+  storageBucket: "simple-firebase-authenti-c309b.appspot.com",
+  messagingSenderId: "502687466770",
+  appId: "1:502687466770:web:75f929ce79ccb616a09fcb",
+  measurementId: "G-82Q5BEGW3T"
+};
+
+export default firebaseConfig;
